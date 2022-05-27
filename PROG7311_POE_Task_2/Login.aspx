2f5a33f6d4b932d07e09b5b211ca355dce27ecdc
= <jareddavidwild4@gmail.com>
2022-05-27 12:32:23 +0200

    login logic

--- PROG7311_POE_Task_2/Login.aspx
+++ PROG7311_POE_Task_2/Login.aspx
@@ -8,15 +8,79 @@
 
     <asp:PlaceHolder runat="server">
         <%: Scripts.Render("~/bundles/modernizr") %>
-    </asp:PlaceHolder>
-     
+    </asp:PlaceHolder> 
     <webopt:bundlereference runat="server" path="~/Content/css" />
     <link href="~/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    
+    <style>
+        .login {
+            height: 70%;
+            max-height: 900px;
+            min-height: 600px;
+            width: 60%;
+            max-width: 600px;
+            min-width: 300px;
+        }
+    </style>
 </head>
 <body class="flex-container centered">
     <div class="container login">
-        <form id="form1" runat="server">
-            <div>
+        <h2>Login</h2>
+        <asp:Label
+            ID="message"
+            ForeColor="Red"
+            runat="server">
+        </asp:Label>
+        <form id="login" class="form" runat="server">
+            <div class="flex-container-vertical spaced-around">
+                <div class="formField">
+                    <label for="UserEmail" class="form-label">Email address</label>
+                    <asp:TextBox
+                        AutoCompleteType="Email"
+                        ID="UserEmail"
+                        CssClass="form-control"
+                        runat="server">
+                    </asp:TextBox>
+                    <asp:RequiredFieldValidator
+                        ID="UsernameRequiredValidator"   
+                        ControlToValidate="UserEmail"  
+                        Display="Dynamic"   
+                        ErrorMessage="Cannot be empty."   
+                        runat="server" />
+                </div>
+                <div class="formField">
+                    <label for="UserPassword" class="form-label">Password</label>
+                    <asp:TextBox
+                        ID="UserPassword"
+                        TextMode="Password"
+                        CssClass="form-control"
+                        runat="server">
+                    </asp:TextBox>
+                    <asp:RequiredFieldValidator
+                        ID="RequiredFieldValidator1"   
+                        ControlToValidate="UserPassword"  
+                        Display="Dynamic"   
+                        ErrorMessage="Cannot be empty."   
+                        runat="server" />
+                </div>
+                <div class="flex-container horizontal-center">
+                    <asp:Button 
+                        CssClass="btn btn-primary mb-3"
+                        OnClick="login_click"
+                        Text="Log In"
+                        runat="server"/>
+                    <div class="form-check">
+                        <asp:CheckBox
+                            ID="ChboxStayLoggedIn"
+                            CssClass="form-check-input"
+                            runat="server"/>
+                        <label
+                            class="form-check-label"
+                            for="ChBoxStayLoggedIn">
+                            Stay logged in
+                        </label>
+                    </div>
+                </div>
             </div>
         </form>
     </div>
--- PROG7311_POE_Task_2/Login.aspx.cs
+++ PROG7311_POE_Task_2/Login.aspx.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
+using System.Web.Configuration;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +13,95 @@ namespace PROG7311_POE_Task_2
 {
     public partial class Login : System.Web.UI.Page
     {
+
+        // entity framework instance for db interactions
+        private StockManagementEntities entities;
+
+        // form fields
+        private TextBox emailBox;
+        private TextBox passwordBox;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            this.emailBox = this.UserEmail;
+            this.passwordBox = this.UserPassword;
+        }
 
+        protected void login_click(Object sender, EventArgs e)
+        {
+            User user = validateUser(this.emailBox.Text, this.passwordBox.Text);
+
+            if(user != null)
+            {
+                // do auth
+            }
+        }
+
+        /// <summary>
+        /// validate entered credentials
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns> returns User or null if invalid </returns>
+        private User validateUser(String email, String password)
+        {
+            // check validity of email
+            if(email.Equals(String.Empty) || email.Equals(null)) {
+                System.Diagnostics.Trace.WriteLine("[ValidatingUser] validation of email failed.");
+                return null;
+            }
+            // check validity of password
+            if (password.Equals(String.Empty) || password.Equals(null)) {
+                System.Diagnostics.Trace.WriteLine("[ValidatingUser] validation of password failed.");
+                return null;
+            }
+
+            try
+            {
+                using (this.entities = new StockManagementEntities())
+                {
+                    User targetUser = this.entities.Users.SingleOrDefault(user => user.Email == email);
+
+                    if(targetUser != null)
+                    {
+                        if(validPassword(password, targetUser.Password))
+                        {
+                            return targetUser;
+                        }
+                        else
+                        {
+                            System.Diagnostics.Trace.WriteLine("[ValidatingUser] Invalid password.");
+                            return null;
+                        }
+                    }
+                    else
+                    {
+                        System.Diagnostics.Trace.WriteLine("[ValidatingUser] Invalid user.");
+                        return null;
+                    }
+                }
+            } 
+            catch(Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex);
+                return null;
+            }
+        }
+
+        private bool validPassword(String EnteredPassword, String targetPassword)
+        {
+            return (targetPassword == ComputeHash(EnteredPassword));
+        }
+
+        private string ComputeHash(string password)
+        {
+            HashAlgorithm sha256 = SHA256CryptoServiceProvider.Create();
+
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+
+            Byte[] hashedBytes = sha256.ComputeHash(inputBytes);
+
+            return BitConverter.ToString(hashedBytes);
         }
     }
 }
